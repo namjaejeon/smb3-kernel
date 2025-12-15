efe1ab806097db43782a7baf8733e617e13cb4cd
Hyunchul Lee <hyc.lee@gmail.com>
2025-12-16 08:50:11 +0900

    ntfs: fix compile errors
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fs/ntfs/ntfs.h
+++ fs/ntfs/ntfs.h
@@ -42,7 +42,7 @@
 
 #define NTFS_MFT_REC_NR_TO_CLU(vol, mft_no) (((u64)mft_no << (vol)->mft_record_size_bits) >> \
 					     (vol)->cluster_size_bits)
-#define NTFS_MFT_REC_NR_TO_FOLIO_IDX(vol, mft_no) (mft_no >> (PAGE_INDEX - \
+#define NTFS_MFT_REC_NR_TO_FOLIO_IDX(vol, mft_no) (mft_no >> (PAGE_SHIFT - \
 							      (vol)->mft_record_size_bits))
 #define NTFS_MFT_REC_NR_TO_FOLIO_OFS(vol, mft_no) (((u64)mft_no << (vol)->mft_record_size_bits) & \
 					     ~PAGE_MASK)
@@ -52,9 +52,9 @@
 #define NTFS_FOLIO_IDX_TO_CLU(vol, idx) (((u64)idx << PAGE_SHIFT) >> \
 					 (vol)->cluster_size_bits)
 #define NTFS_CLU_TO_FOLIO_IDX(vol, clu) (((u64)clu << (vol)->cluster_size_bits) >> \
-					 PAGE_SHIFT
+					 PAGE_SHIFT)
 #define NTFS_CLU_TO_FOLIO_OFFS(vol, clu) (((u64)clu << (vol)->cluster_size_bits) & \
-					 ~PAGE_MASK
+					 ~PAGE_MASK)
 
 enum {
 	NTFS_BLOCK_SIZE		= 512,
